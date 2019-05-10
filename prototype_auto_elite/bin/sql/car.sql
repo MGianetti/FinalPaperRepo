9420a9953f6e9d6566a8d5de1d2a712223d83f25
mgianetti <mateusgianetti1996@gmail.com>
2019-05-10 16:25:54 -0300

    changed folder named

--- prototype_auto_elite/bin/sql/car.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE car(
-    plate VARCHAR(32) PRIMARY KEY, 
-    is_mercosul BOOLEAN NOT NULL,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP
-    holder VARCHAR(64)
-    FOREIGN KEY ("holder") REFERENCES client(cpf)
-);
