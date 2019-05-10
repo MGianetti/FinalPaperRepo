d55a9633758f4a09ee816a1376845911463941f0
mgianetti <mateusgianetti1996@gmail.com>
2019-05-10 17:32:28 -0300

    Changed menu size

--- Project/front-auto-elite/src/App.js
+++ Project/front-auto-elite/src/App.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom'
 import NavBar from './components/navBar';
 import FixedMenuBar from './components/fixedMenuBar';
+import Grid from '@material-ui/core/Grid';
 import './App.css';
 
 function App() {
     return (
-		<React.Fragment>
-			<NavBar />
-			<FixedMenuBar/>
-		</React.Fragment>
+			<React.Fragment>
+				<NavBar />
+				<Grid container spacing={24}>
+					<Grid item xs={3}>
+						<div className="column">
+							<FixedMenuBar/>
+						</div>
+					</Grid>
+				</Grid>
+			</React.Fragment>
+
     );
 }
 
